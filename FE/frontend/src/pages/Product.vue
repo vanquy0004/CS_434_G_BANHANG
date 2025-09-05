8517bc41c60d79acb5c7719ea8b9fcbafe2dfc28
quy0004 <vanquy0004@gmail.com>
2025-09-05 11:56:11 +0700

    sua tieu de

--- FE/frontend/src/pages/Product.vue
+++ FE/frontend/src/pages/Product.vue
@@ -2,7 +2,7 @@
     <div>
         <h1>Welcome to Shop LEOG</h1>
         <p>This is my products</p>
-        <lable>Trang san pham tai branch giaodien/trang-san-pham</lable>
+        <lable>Trang Nguyen Dinh Vi code</lable>
     </div>
 </template>
 
