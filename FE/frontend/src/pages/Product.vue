481dd77cf44e3021f29c070d4be09b627da448da
quy0004 <vanquy0004@gmail.com>
2025-09-05 11:35:27 +0700

    create product page

---
+++ FE/frontend/src/pages/Product.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Welcome to Shop LEOG</h1>
+        <p>This is my products</p>
+        <lable>Trang san pham</lable>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+};
+</script>
