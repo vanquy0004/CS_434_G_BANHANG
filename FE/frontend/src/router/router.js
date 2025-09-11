b5ad4f7c410edc915a509d8f5cf6dcc68fa59ff3
quy0004 <vanquy0004@gmail.com>
2025-09-11 16:46:49 +0700

    tao trang dang nhap

--- FE/frontend/src/router/router.js
+++ FE/frontend/src/router/router.js
@@ -2,11 +2,11 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import About from "../pages/About.vue";
 import Product from "../pages/Product.vue";
-
+import Login from "../pages/Login.vue";
 const routes = [
   { path: "/about", component: About },
   { path: "/", component: Home },
-  // { path: "/login", component: Login },
+  { path: "/login", component: Login },
   { path: "/product", component: Product },
 ];
 
